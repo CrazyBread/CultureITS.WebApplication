d365bd4848c134240f8dd5f8ff3b51d5958d79b0
Vladislav Moiseev <v.moiseev94@gmail.com>
2014-02-24 19:53:59 +0400

    1) Изменены стили 2) Добавлены первые изображения 3) Исправлено меню 4) Изменены странице Home контроллера
    Signed-off-by: Vladislav Moiseev <v.moiseev94@gmail.com>

--- CultureITS/Models/Context/DataContext.cs
+++ CultureITS/Models/Context/DataContext.cs
@@ -29,7 +29,7 @@ namespace CultureITS.Models.Context
                 context.SaveChanges();
 
                 context.MenuItems.Add(new MenuItem() { Order = 1, Title = "Профиль", Controller = "Account", Action = "Index", AccessMask = (1 << (int)AccountStatus.Student) + (1 << (int)AccountStatus.Admin) });
-                context.MenuItems.Add(new MenuItem() { Order = 0, Title = "Администрирование", Controller = "Admin", Action = "Index", AccessMask = 1 << (int)AccountStatus.Admin });
+                context.MenuItems.Add(new MenuItem() { Order = 0, Title = "Администрирование", Area = "Admin", Controller = "Home", Action = "Index", AccessMask = 1 << (int)AccountStatus.Admin });
                 context.MenuItems.Add(new MenuItem() { Order = 0, Title = "О системе", Controller = "Home", Action = "About", AccessMask = -1 });
                 context.SaveChanges();
             }
