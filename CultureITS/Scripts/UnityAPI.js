952198971f6167b68d4877dc966208dad4bab057
Vladislav Moiseev <v.moiseev94@gmail.com>
2014-03-13 23:40:51 +0400

    1) Скрипты (альфа API) вынесены в отдельный файл

---
+++ CultureITS/Scripts/UnityAPI.js
@@ -0,0 +1,32 @@
+﻿//main function
+Request = function (command, data, result) {
+    $.post("/Api/" + command, data, result);
+};
+
+//command function
+markGameObject = function (id) {
+    Request("markGameObject", { id: id }, function (data) {
+        if (data.success)
+            alert('ok');
+        else
+            alert(data.message);
+    });
+};
+
+checkGameObject = function (id) {
+    Request("checkGameObject", { id: id }, function (data) {
+        if (data.success)
+            alert(data.state);
+        else
+            alert(data.message);
+    });
+};
+
+getGameObjectInfo = function (id, full) {
+    Request("getGameObjectInfo", { id: id, full: full }, function (data) {
+        if (data.success)
+            alert(data.name + " " + data.canNotified + " " + data.description);
+        else
+            alert(data.message);
+    });
+};
