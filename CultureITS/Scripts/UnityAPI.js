713b62b74ac373b69cc16dfa41b2143c0aec28b6
Vladislav Moiseev <v.moiseev94@gmail.com>
2014-03-21 00:55:35 +0400

    1) Реализована альфа-версия интерфейса тестирования (без иллюстраций и с кривой вёрсткой). 2) Подправлена таблица стилей. 3) Версия проекта Unity -- 4.
    Signed-off-by: Vladislav Moiseev <v.moiseev94@gmail.com>

--- CultureITS/Scripts/UnityAPI.js
+++ CultureITS/Scripts/UnityAPI.js
@@ -9,7 +9,16 @@ Request = function (command, data, result) {
     });
 };
 
-//command function
+//unity interaction
+showTestPopup = function (id) {
+    if (testConfig["State"] == false) {
+        getTestInfo(id);
+    } else {
+        alert("Вы уже проходите тестирование");
+    }
+};
+
+//gameObject subsystem
 markGameObject = function (id) {
     Request("markGameObject", { id: id }, function (data) {
         if (data.success)
@@ -36,3 +45,151 @@ getGameObjectInfo = function (id, full) {
             alert(data.message);
     });
 };
+
+
+//test subsystem
+var testConfig = [];
+testConfig["State"] = false;
+testConfig["Id"] = 0;
+
+getTestInfo = function (id) {
+    Request("getTestInfo", { id: id }, function (data) {
+        if (data.success) {
+            testConfig["Id"] = id;
+            testConfig["QuestionsCount"] = data.questionsCount;
+
+            $('#TestHeader').text(data.title + " (" + data.topic + ")");
+
+            $('#TestStartTitle').text(data.title);
+            $('#TestStartTopic').text(data.topic);
+            $('#TestStartAuthor').text(data.author);
+            $('#TestStartQuestionsCount').text(data.questionsCount);
+            $('#TestStartResultsCount').text(data.resultsCount);
+            $('#TestStartDate').text((data.resultsCount > 0) ? data.dateLastResult : "-");
+            $('#TestStartOpened').text((data.isOpened) ? "Да" : "Нет");
+            $('#TestMainQuestionCount').text(data.questionsCount);
+
+            $('#TestStart').show();
+            $('#TestMain').hide();
+            $('#TestResult').hide();
+
+            openPopup("#TestPopup");
+        } else {
+            alert(data.message);
+        }
+    });
+}
+
+startTest = function (id) {
+    Request("startTest", { id: id }, function (data) {
+        if (data.success) {
+            testConfig["State"] = true;
+            testConfig["SessionId"] = data.testSessionId;
+            testConfig["QuestionsLeft"] = data.questionLeft;
+            testConfig["CurrentQuestion"] = testConfig["QuestionsCount"] - testConfig["QuestionsLeft"] + 1;
+
+            getTestQuestion(testConfig["SessionId"], testConfig["CurrentQuestion"]);
+
+            $('#TestStart').hide();
+            $('#TestMain').show();
+            $('#TestResult').hide();
+        } else {
+            alert(data.message);
+        }
+    });
+}
+
+getTestQuestion = function (sessionId, questionNumber) {
+    Request("getTestQuestion", { testSessionId: sessionId, questionNumber: questionNumber }, function (data) {
+        if (data.success) {
+            $('#TestMainQuestionNumber').text(data.number);
+            $('#TestMainText').text(data.text); // +++++ Application
+            testConfig["MultiChoise"] = data.allowMultiChoise;
+
+            $('#TestMainAnswers *').remove();
+            data.answers.forEach(function (item) {
+                $('#TestMainAnswers').append(
+                    $('<li>').append(
+                        $('<input>').attr('id', 'question' + item.id).attr('value', item.id).attr('name', 'testAnswer').attr('type', testConfig["MultiChoise"] ? 'checkbox' : 'radio')).append(item.text)
+                );
+                // ++++++ Application
+            });
+        } else {
+            alert(data.message);
+        }
+    });
+}
+
+setTestAnswer = function (sessionId, questionNumber, answers) {
+    Request("setTestAnswer", { testSessionId: sessionId, questionNumber: questionNumber, answersNumbers: answers }, function (data) {
+        if (data.success) {
+            testConfig["QuestionsLeft"]--;
+            testConfig["CurrentQuestion"] = data.questionNext;
+
+            if (data.result == 1) {
+                getTestQuestion(testConfig["SessionId"], testConfig["CurrentQuestion"]);
+            } else {
+                getTestResult(testConfig["SessionId"]);
+
+                $('#TestStart').hide();
+                $('#TestMain').hide();
+                $('#TestResult').show();
+
+                testConfig["State"] = false;
+            }
+        } else {
+            alert(data.message);
+        }
+    });
+}
+
+getTestResult = function (sessionId) {
+    Request("getTestResult", { testSessionId: sessionId }, function (data) {
+        if (data.success) {
+            $table = $('#TestResultTable');
+            data.questionsResults.forEach(function (item) {
+                $table.append(
+                    $('<tr>')
+                        .append($('<td>').text(item.number))
+                        .append($('<td>').text(item.result))
+                        .append($('<td>').text(item.maxResult))
+                );
+            });
+        } else {
+            alert(data.message);
+        }
+    });
+}
+
+$('#TestStartButton').click(function () {
+    if ((!testConfig["State"]) && (testConfig["Id"] > 0)) {
+        startTest(testConfig["Id"]);
+    } else {
+        alert("Невозможно начать тест, так как вы уже проходите тестирование или не выбрали ни одного теста.");
+    }
+});
+
+$('#TestMainButton').click(function () {
+    if (!testConfig["State"]) {
+        alert('Вы не проходите тестирование.');
+        return;
+    }
+
+    var $items = $("#TestMainAnswers input[type=" + ((testConfig["MultiChoise"]) ? "checkbox" : "radio") + "]:checked");
+    var answersCount = $items.size();
+    if (answersCount == 0) {
+        alert('Выберите хотя бы один правильный ответ.');
+        return;
+    }
+
+    var answersArray = new Array();
+    $items.each(function (i) {
+        answersArray.push($(this).val());
+    });
+
+    setTestAnswer(testConfig["SessionId"], testConfig["CurrentQuestion"], answersArray);
+});
+
+$(".popup_close, .popup").click(function () {
+    testConfig["State"] = false;
+});
--- CultureITS/Scripts/UserScript.js
+++ CultureITS/Scripts/UserScript.js
@@ -1,24 +1,35 @@
 $(document).ready(function(){
-    $(".fancybox").click(function(){
-        $($(this).attr("href")).show();
-        $(".popup").show();
+    $(".fancybox").click(function () {
+        openPopup($(this).attr("href"));
+    });
 
-        if (typeof u != 'undefined') {
-            $(u.getUnity()).width(0);
-            $(u.getUnity()).height(0);
-            $("#unityPlayer").height(700);
-        }
-    });
     $(".popup_close, .popup").click(function(){
-        $(".popup").hide();
-        $(".popup_window").hide();
-        
-        if (typeof u != 'undefined') {
-            $(u.getUnity()).width(1170);
-            $(u.getUnity()).height(700);
-        }
+        closePopup();
     });
+
     $(".popup_window").click(function(e){
         e.stopPropagation();
     });
 });
+
+openPopup = function (pWindow) {
+    $(".popup_window").hide();
+    $(pWindow).show();
+    $(".popup").show();
+
+    if (typeof u != 'undefined') {
+        $(u.getUnity()).width(0);
+        $(u.getUnity()).height(0);
+        $("#unityPlayer").height(700);
+    }
+}
+
+closePopup = function () {
+    $(".popup").hide();
+    $(".popup_window").hide();
+
+    if (typeof u != 'undefined') {
+        $(u.getUnity()).width(1170);
+        $(u.getUnity()).height(700);
+    }
+}
